698bca378aad49513fbbebcad28e99132713e279
Guillermina <guillecarrascosuarez@gmail.com>
2024-06-28 17:45:56 -0300

    Personaje del principe

---
+++ Models/Principe.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ProyectoJuegoDeRol.Models
+{
+    public class Principe
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int Carisma { get; set; }
+        public int Inteligencia { get; set; }
+        public int Atractivo { get; set; }
+        public Hobbie Hobbie { get; set; }
+
+    }
+}
